8707b7a2a5485e0916ca2bd8acecf741bf450723
Gabrielende5 <gabrielende53@gmail.com>
2023-10-28 09:47:29 -0300

    começando portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css"
+function Navbar(){
+    return(
+        <ul id="navbar">
+            <li>João Vitor</li>
+            <li id="home">Home</li>
+            <li>About me</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    )
+}
+
+export default Navbar;
