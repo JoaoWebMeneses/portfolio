9d90839a299b969b9f769321c7896f25ae050100
JoaoWebMeneses <joaowebmeneses@gmail.com>
2023-12-09 09:45:47 -0300

    push portfolio

--- src/pages/Aboutme.jsx
+++ src/pages/Aboutme.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Root from '../components/Root';
+import "./Aboutme.css"
 
 export default function Aboutme() {
   return (
-    <>
-        <div>About me</div>
-    </>
+    <div>
+      <h1>About Me!</h1>
+      <div className="paragrafos">
+        <p>Comecei a programar em 2019, com 9 anos, fazendo joguinhos no Construct 3 e no Scratch.</p>
+        <p>Aos 10 anos, comecei a aprender Python e fazer projetos básicos, aprimorando meus conhecimentos</p>
+      </div>
+      <div className="linguagens">
+        <img className="imglinguagens" src='https://cdn-icons-png.flaticon.com/128/1822/1822920.png'></img>
+        <img className="imglinguagens" src='https://cdn-icons-png.flaticon.com/128/5968/5968292.png'></img>
+        <img className="imglinguagens" src='https://cdn-icons-png.flaticon.com/128/10832/10832132.png'></img>
+        <img className="imglinguagens" src='https://cdn-icons-png.flaticon.com/128/6132/6132222.png'></img>
+      </div>
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Root from '../components/Root';
 import "./Home.css";
-import { FaGithub } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
-import { FaInstagramSquare } from "react-icons/fa";
 
 function Home() {
   return (
@@ -11,9 +8,9 @@ function Home() {
       <h1 id='name'>Hi, I'm João Vitor</h1>
       <div id='socialmedia'>
         <div className='icons'>
-          <FaGithub size={30} style={{width: '50px'}}/>
-          <FaLinkedin size={30} style={{witdh: '50px'}}/>
-          <FaInstagramSquare size={30} style={{width: '50px'}}/>
+          <a href='https://github.com/JoaoWebMeneses'><img className="img" src='https://cdn-icons-png.flaticon.com/128/2111/2111425.png'/></a>
+          <a href='https://github.com/JoaoWebMeneses'><img className="img" src="https://cdn-icons-png.flaticon.com/128/3938/3938052.png"/></a>
+          <a href='https://github.com/JoaoWebMeneses'><img className="img" src="https://cdn-icons-png.flaticon.com/128/61/61109.png"/></a>
         </div>
       </div>
     </div>
