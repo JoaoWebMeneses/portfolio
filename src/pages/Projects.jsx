d6c754a808c40a853f19a4023a7176ab1cf7059d
JoaoWebMeneses <111091586+JoaoWebMeneses@users.noreply.github.com>
2023-12-18 14:49:29 -0300

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Projects.css"
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='pagprojects'>
+      <h1>Projects</h1>
+      <div className='projects'>
+        <br/>
+        <div className='fluxodecaixa'>
+          <h2>Fluxo de Caixa</h2>
+          <button id='bfluxo'><a href="https://fluxodecaixa-txlz.vercel.app">Visit</a></button>
+        </div>
+        <br/>
+        <div className='wikireceitas'>
+          <h2>WikiReceitas</h2>
+          <button id='bwiki'><a href="https://wikireceitas.vercel.app">Visit</a></button>
+        </div>
+      </div>
+    </div>
   )
 }
 
